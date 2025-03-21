533f1bff22bd78cb31a60ae5543f59fbedd0393e
JanuaryCJ <janstudyacc@gmail.com>
2025-03-22 02:14:02 +0800

    Add destructuring for email and password in login route

--- auth/authRoutes.js
+++ auth/authRoutes.js
@@ -35,6 +35,8 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/login",async(req,res)=>{
+
+    const {email,password} = req.body;
     try{
         if (!email || !password){
             return res.status(400).json({error:"Email and password are required!"});
